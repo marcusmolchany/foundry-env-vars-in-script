1e55bd31dd0ad2bfdcc4a2bf66b47140a39810c9
Marcus Molchany <marcusmolchany@gmail.com>
2022-08-01 17:40:00 -0700

    add local env, script, and steps to run test

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # foundry-env-vars-in-script
 
-testing foundry/forge loading bash env vars into script using ffi
+testing foundry/forge loading bash env vars into script using environment cheats
+
+## run the script
+
+The following script will:
+
+1. load the env vars in [`.env.local`](./.env.local)
+1. echo the bash env var `$STRING_ENV_VAR` (to prove that it is loaded)
+1. run the forge script
+
+```sh
+$ source .env.local && echo $STRING_ENV_VAR && forge script script/Contract.s.sol -vvvvv
+```
--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "forge-std/console2.sol";
 
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        bool boolEnvVar = vm.envBool("BOOL_ENV_VAR");
+        console2.log("BOOL_ENV_VAR", boolEnvVar);
+        bool boolEnvVar2 = vm.envBool("BOOL_ENV_VAR_2");
+        console2.log("BOOL_ENV_VAR_2", boolEnvVar2);
+        string memory stringEnvVar = vm.envString("STRING_ENV_VAR");
+        console2.log("STRING_ENV_VAR", stringEnvVar);
     }
 }
